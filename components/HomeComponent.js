1d92c0ad302c2dd2ae105023f0bd95819861e341
Jason Bettyas <jbettyas@gmail.com>
2019-03-09 12:57:47 -0800

    homes skillet

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -15,7 +15,7 @@ class Home extends Component {
         
         return (
             <View>
-                <Text>This is Home Component</Text>
+                <Text>This is Homes skillet Component</Text>
             </View>
         );
     }
